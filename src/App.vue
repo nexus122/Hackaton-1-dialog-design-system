53c3846788deb332b740a24f0c3ac956f75c7822
nexus122 <juan-pablo122@hotmail.es>
2022-05-17 00:15:06 +0200

    Generación de estructura dinamica y estilos

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,78 @@
 <script setup>
+import DialogComponent from "./components/DialogComponent.vue";
+</script>
 
-import Dialog from "./components/Dialog.vue";
-
+<script>
+export default {
+  name: "App",
+  components: {
+    DialogComponent,
+  },
+  data() {
+    return {
+        warning: {
+          visible: false,
+          icon: '<i class="fa-solid fa-exclamation"></i>',
+          background_color: 'rgba(255, 217, 18, 0.12)',
+          color: '#FFD912',
+          title: '¡Cuidado!',
+          text: 'No podrás volver atrás',
+          buttons: `
+          <div class="btn-group">
+            <button class="btn primary">Borrar</button>
+            <button class="btn primary outline">Cancelar</button>
+          </div>`
+        },
+        succes: {
+          visible:false,
+          icon: '<i class="fa-solid fa-check"></i>',
+          background_color: 'rgba(39, 174, 96, 0.12)',
+          color: 'rgba(39, 174, 96, 1)',
+          title: '¡Bien hecho!',
+          text: 'Todo ha ido bien 😄',
+          buttons: `
+          <div class="btn-group">
+            <button class="btn primary">Cerrar Pestaña</button>
+          </div>
+          `          
+        },
+        error:{
+          visible:false,
+          icon: '<i class="fa-solid fa-times"></i>',
+          background_color: 'rgba(192, 57, 43, 0.12)',
+          color: 'rgba(192, 57, 43, 1)',
+          title: '¡Error!',
+          text: 'Algo ha ido mal 😭',
+          buttons: ``
+        },        
+    };
+  },
+  methods: {
+    showWarning() {
+      this.warning.visible = true;
+    },
+    showSucces() {
+      this.succes.visible = true;
+    },
+    showError() {
+      this.error.visible = true;
+    },
+  },  
+}
 </script>
 
 <template>
   <main>
 
     <div class="buttons">
-      <button class="btn warning">Abrir warning</button>
-      <button class="btn succes">Abrir Succes</button>
-      <button class="btn error">Abrir Error</button>
+      <button class="btn primary outline" @click="showWarning">Mostrar warning</button>
+      <button class="btn primary outline" @click="showSucces">Mostrar succes</button>
+      <button class="btn primary outline" @click="showError">Mostrar error</button>
     </div>
-    <br>
-    <Dialog/>
+    
+    <DialogComponent :dialogInfo="warning"/>
+    <DialogComponent :dialogInfo="succes"/>
+    <DialogComponent :dialogInfo="error"/>
 
   </main>
 </template>
@@ -23,4 +82,47 @@ import Dialog from "./components/Dialog.vue";
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 /* Importamos la tipografia Roboto */
 @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+
+.buttons {
+  display: flex;    
+  margin-bottom: 1em;
+  margin-top: 1em;
+  gap: 1em;
+}
+
+/* Buttons de Dialogs */
+
+.title {
+  text-align: center;
+}
+.title p {
+  font-family: "Roboto";
+  font-style: normal;
+  margin-top: 8px;
+}
+
+.btn-group {
+  display: flex;
+  gap: 1em;
+}
+
+.btn {
+  cursor: pointer;
+  padding: 10px 20px 10px 20px;
+  border-radius: 8px;
+  min-width: 44px;  
+}
+
+.btn.primary {
+  background: #6e41e2;
+  border: 1px solid #6e41e2;
+  color: white;
+}
+
+.btn.primary.outline {
+  background: transparent;
+  border: 1px solid #6e41e2;
+  color: #6e41e2;
+}
+
 </style>
---
+++ src/components/DialogComponent.vue
@@ -0,0 +1,137 @@
+<template>
+  <div class="dialog" v-if="dialogInfo.visible">
+    <!-- BTN CLOSE -->
+    <div class="close">
+      <i class="fa-solid fa-xmark"></i>
+    </div>
+    <!-- Contenido del popup -->
+    <div class="popUpContent">
+      <!-- Dialog icon -->
+      <div class="icon" :style="{color:dialogInfo.color, background: dialogInfo.background_color}">
+        <span class="icono" v-html="dialogInfo.icon"></span>
+      </div>
+      <!-- Title and text -->
+      <div class="title">
+        <h1>{{dialogInfo.title}}</h1>
+        <p>{{dialogInfo.text}}</p>
+      </div>
+
+      <div v-html="dialogInfo.buttons"></div>
+      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DialogComponent",
+  emits: ['close'],
+  props: {
+    dialogInfo: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+/* General */
+.dialog {
+  width: 481px;
+  height: 314px;
+  border-radius: 8px;
+  
+  box-shadow: 0px 4px 4px rgba(51, 51, 51, 0.04), 0px 4px 16px rgba(51, 51, 51, 0.08);
+
+  font-family: "Montserrat";
+  font-style: normal;
+}
+
+/* Contenido del popup */
+.popUpContent {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+/* BTN cerrar */
+.close {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background: #6e41e2;
+  color: white;
+  width: 44px;
+  height: 44px;
+
+  position: relative;
+  left: calc(100% - 44px - 12px);
+  top: 12px;
+  border-radius: 100px;
+
+  cursor: pointer;
+
+  font-size: 1.5em;
+  box-sizing: border-box;
+}
+
+/* Dialog Icon */
+.icon {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 96px;
+  height: 96px;
+  right: 0px;
+  top: 0px;
+  border-radius: 100%;
+
+  /* Colors / Star */
+  /*background: rgba(255, 217, 18, 0.12);*/
+
+}
+
+.icono {
+  /* 56px / Attention */
+  /*color: #FFD912;*/
+  font-size: 2em;
+}
+
+.title {
+  text-align: center;
+}
+.title p {
+  font-family: "Roboto";
+  font-style: normal;
+  margin-top: 8px;
+}
+
+.btn-group {
+  display: flex;
+  gap: 1em;
+}
+
+.btn {
+  cursor: pointer;
+  padding: 10px 20px 10px 20px;
+  border-radius: 8px;
+  min-width: 44px;  
+}
+
+.btn.primary {
+  background: #6e41e2;
+  border: 1px solid #6e41e2;
+  color: white;
+}
+
+.btn.primary.outline {
+  background: transparent;
+  border: 1px solid #6e41e2;
+  color: #6e41e2;
+}
+
+</style>
